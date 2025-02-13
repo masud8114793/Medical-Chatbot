e53cdb4841e51f1cd2bac6f45ee140334dd3b710
masud8114793 <masud8114793@gmail.com>
2025-02-13 18:04:40 +0600

    added file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import  find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Masud",
+    author_email="mparvez@unicef.org",
+    packages=find_packages(),
+    install_requires=[]
+)
